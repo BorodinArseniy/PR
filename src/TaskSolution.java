7e6c48c216c2858b2ff8b65abf5da92d0ae4f387
Arseniy <b9692861615@gmail.com>
2022-11-09 17:25:23 +0300

    1

--- src/TaskSolution.java
+++ src/TaskSolution.java
@@ -1,3 +1,11 @@
 public class TaskSolution {
-    
+    public static void main(String[] args) {
+        //Дан массив пар названий книг и авторов,
+        //необходимо привести каждую пару в строку вида: «Название книги» И.О.Автора.
+        String[] books = {"IT", "1984", "Onegin"};
+        String[] authors = {"Stephen King", "George Orwell", "Alexandr Pushkin"};
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i] + " - " + authors[i]);
+        }
+    }
 }
